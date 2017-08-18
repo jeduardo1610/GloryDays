f473a2876f49ade5d9279a8dca4bde616a43ead1
Jorge Eduardo <jeduardo1601@gmail.com>
2017-08-18 11:32:41 -0500

    Creating methods to ask the user for permission

--- GloryDays/ViewController.swift
+++ GloryDays/ViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import AVFoundation
+import Photos
+import Speech
 
 class ViewController: UIViewController {
 
@@ -23,6 +26,90 @@ class ViewController: UIViewController {
 
 
     @IBAction func askForPermission(_ sender: UIButton) {
+        self.askForPhotosPermission()
+    }
+    
+    func askForPhotosPermission(){
+        PHPhotoLibrary.requestAuthorization { [unowned self](authStatus) in
+            
+            DispatchQueue.main.async {
+                switch authStatus {
+                    
+                case .authorized:
+                    self.askForRecordPermission()
+                    
+                case .denied:
+                    self.showDialog(title: "Glory Days", message: "Please go to your device settings to enable permission for this app.")
+                    
+                case .notDetermined:
+                    self.showDialog(title: "Glory Days", message: "Please go to your device settings to enable permission for this app.")
+                    
+                default:
+                    break
+                    
+                }
+            }
+            
+        }
+    }
+    
+    func askForRecordPermission(){
+        
+        AVAudioSession.sharedInstance().requestRecordPermission { [unowned self](allowed) in
+            DispatchQueue.main.async {
+                switch allowed {
+                case true :
+                    self.askForTranscriptionPermission()
+                case false:
+                    self.showDialog(title: "Glory Days", message: "Please go to your device settings to enable permission for this app.")
+                }
+            }
+        }
+        
+    }
+    
+    func askForTranscriptionPermission(){
+        
+        SFSpeechRecognizer.requestAuthorization { [unowned self](authStatus) in
+            
+            DispatchQueue.main.async {
+                
+                switch authStatus {
+                case .authorized:
+                    self.authorizationComplete()
+                case .denied:
+                    self.showDialog(title: "Glory Days", message: "Please go to your device settings to enable permission for this app.")
+                case .notDetermined:
+                    self.showDialog(title: "Glory Days", message: "Please go to your device settings to enable permission for this app.")
+                default:
+                    break
+                }
+                
+            }
+            
+        }
+        
+    }
+    
+    func authorizationComplete(){
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func showDialog(title: String?, message : String?){
+        
+        if let title = title, let message = message {
+            let alertController : UIAlertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            
+            let okAction : UIAlertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            
+            alertController.addAction(okAction)
+            
+            self.present(alertController, animated: true, completion: nil)
+        } else {
+            showDialog(title: "Glory Days", message: "Something went terribly wrong")
+        }
+        
     }
     
 }
